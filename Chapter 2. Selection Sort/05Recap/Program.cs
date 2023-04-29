416e3f61d551af70843ed90fcbf788c522115e54
Eliot <45435800+eliot-geek@users.noreply.github.com>
2023-04-29 20:01:19 +0300

    Chapter 2. Selection Sort

---
+++ Chapter 2. Selection Sort/05Recap/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _05Recap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Recap
+            • Your computer’s memory is like a giant set of drawers.
+            • When you want to store multiple elements, use an array or a list.
+            • With an array, all your elements are stored right next to each other.
+            • With a list, elements are strewn all over, and one element stores the address of the next one.
+            • Arrays allow fast reads.
+            • Linked lists allow fast inserts and deletes.
+            • All elements in the array should be the same type (all ints, all doubles, and so on).*/
+            
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
